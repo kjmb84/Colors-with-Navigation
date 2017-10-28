4134ac78a15e32b623ea2c8c85cf4cf21d76ce67
Kean <kjmb84@mail.missouri.edu>
2017-10-27 19:41:56 -0500

    fin

---
+++ Colors/Color.swift
@@ -0,0 +1,14 @@
+//
+//  Color.swift
+//  ColorsWithNavigation
+//
+//  Created by Kean M on 10/27/17.
+//  Copyright © 2017 Kean M. All rights reserved.
+//
+
+import UIKit
+
+struct Color {
+    var name: String
+    var uiColor: UIColor
+}
---
+++ Colors/ColorsViewController.swift
@@ -0,0 +1,70 @@
+//
+//  ColorsViewController.swift
+//  Colors
+//
+//  Created by Kean M on 10/20/17.
+//  Copyright © 2017 Kean M. All rights reserved.
+//
+
+import UIKit
+
+class ColorsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    //var colors = ["red": UIColor.red, "orange": UIColor.orange, "yellow": UIColor.yellow, "green": UIColor.green, "blue": UIColor.blue, "purple": UIColor.purple, "brown": UIColor.brown]
+    
+    var colors = [Color(name: "red", uiColor: UIColor.red),
+                  Color(name: "orange", uiColor: UIColor.orange),
+                  Color(name: "yellow", uiColor: UIColor.yellow),
+                  Color(name: "green", uiColor: UIColor.green),
+                  Color(name: "blue", uiColor: UIColor.blue),
+                  Color(name: "purple", uiColor: UIColor.purple),
+                  Color(name: "brown", uiColor: UIColor.brown)]
+    
+    @IBOutlet weak var colorsTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return colors.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "colorCell", for: indexPath)
+        let color = Array(colors)[indexPath.row]
+        cell.textLabel?.text = color.name
+        cell.backgroundColor = color.uiColor
+        cell.selectionStyle = .none
+        
+        return cell
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let destination = segue.destination as? ColorDetailViewController,
+            let row = colorsTableView.indexPathForSelectedRow?.row {
+            destination.color = colors[row]
+        }
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
